c9567ca75147acd4b07b86a16df72ccbee4a4c64
Ahmad Hozien <ahmedhozien61@yahoo.com>
2021-01-04 13:36:28 +0200

    new editS

---
+++ assets/js/code.js
@@ -0,0 +1,7 @@
+$(".do-nicescrol").niceScroll({
+	cursorcolor: "#748A96",
+	cursorwidth: "9px",
+	cursorborder: "1px solid #707070",
+	cursorborderradius: "8px",
+	cursorfixedheight: 60
+});  // a world full of color!
